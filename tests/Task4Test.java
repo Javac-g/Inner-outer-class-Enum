cdf04514b56246592a0475e3ac4ea374c7601fbd
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2025-06-27 00:43:59 +0300

    Update and rename Tas4Test.java to Task4Test.java
    typo update

--- tests/Task4Test.java
+++ tests/Task4Test.java
@@ -14,7 +14,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class Tas4Test {
+class Task4Test {
 
 	private static String LINE_TYPE = "jom.com.softserve.s3.task4.LineType";
 
